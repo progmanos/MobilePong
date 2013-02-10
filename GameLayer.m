3999ec3ac000fa0f88c45365b6704fcdd8ae0e96
Rashad Tatum <tatum.rashad@gmail.com>
2013-02-10 13:48:50 -0500

    Syncing github with local changes

--- GameLayer.m
+++ GameLayer.m
@@ -1,4 +1,4 @@
-//
+///Users/rashad/Kobold2D/Kobold2D-2.0.4/Kobold2D.xcworkspace
 //  GameLayer.m
 //  Pong
 //
@@ -8,6 +8,8 @@
 
 #import "GameLayer.h"
 
+#define ballSpeedX 4
+#define ballSpeedY 4
 
 @implementation GameLayer
 
@@ -19,19 +21,32 @@
 -(id) init {
     if ((self=[super init])) {
         CCLOG(@"%@: %@", NSStringFromSelector(_cmd), self);
-        
+        CCSprite *background = [CCSprite spriteWithFile:@"court.png"];
         pongball = [Ball spriteWithFile:@"ball1.png"];
-        [self addChild:pongball z:0 tag:1];
+        [self addChild:background z:0 tag:0];
+        [self addChild:pongball z:1 tag:1];
         
         CGSize screenSize=[CCDirector sharedDirector].winSize;
-       
+        [background setPosition:CGPointMake(screenSize.width/2, screenSize.height/2)];
         
-        pongball.position=CGPointMake(screenSize.width / 2, screenSize.height / 2);
+        pongball.position = CGPointMake(screenSize.width / 2, screenSize.height / 2);
+        pongball.velocity = CGPointMake(ballSpeedX, ballSpeedY);
+        [self scheduleUpdate];
     }
+    
     return self;
 }
 
--void
+-(void)update:(ccTime)delta
+{
+    pongball.position = CGPointMake(pongball.position.x + pongball.velocity.x, pongball.position.y + pongball.velocity.y);
+    if(pongball.position.x < 15 || pongball.position.x > 305)
+        pongball.velocity = CGPointMake(-pongball.velocity.x, pongball.velocity.y);
+    
+    if(pongball.position.y < 15 || pongball.position.y > 445)
+        pongball.velocity = CGPointMake(pongball.velocity.x, -pongball.velocity.y);
+
+}
 
 -(void) dealloc
 {
