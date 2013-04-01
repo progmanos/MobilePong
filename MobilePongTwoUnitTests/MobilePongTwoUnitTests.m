a91e877bc2e7854fd72811587e408c850ca4591c
Rashad Tatum <tatum.rashad@gmail.com>
2013-04-01 07:20:28 -0400

    new physics

---
+++ MobilePongTwoUnitTests/MobilePongTwoUnitTests.m
@@ -0,0 +1,32 @@
+//
+//  MobilePongTwoUnitTests.m
+//  MobilePongTwoUnitTests
+//
+//  Created by Rashad on 3/19/13.
+//
+//
+
+#import "MobilePongTwoUnitTests.h"
+
+@implementation MobilePongTwoUnitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MobilePongTwoUnitTests");
+}
+
+@end
