06187f7c73203553b9c4ce4488a18dd5720cd476
progmanos <tatum.rashad@gmail.com>
2013-04-30 03:15:48 -0300

    Fixed Game Over Bug
    Fixed bug that occurs when the other team is the server and we are the client, and they win the game. This bug would cause the pause message to be displayed after the "Main Menu" option has been selected from the GameOver pop-up.

--- cocos2d-2.x-ARC-iOS/MultiplayerLayer.m
+++ cocos2d-2.x-ARC-iOS/MultiplayerLayer.m
@@ -826,6 +826,7 @@ typedef enum {
             pauseAlert = [[UIAlertView alloc] initWithTitle:@"Player 1 has paused the game" message:nil delegate:self cancelButtonTitle:@"Quit Game" otherButtonTitles:nil, nil];
             [pauseAlert show];
             pauseAlertDisplayed = TRUE;
+            [[CCDirector sharedDirector] pause];
         }
            
        }
